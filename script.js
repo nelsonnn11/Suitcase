9acfd706311a2d2d613e1721a33178eb5fc24905
NelsonO1 <90624280+nelsonnn11@users.noreply.github.com>
2023-01-10 15:43:30 +0000

    all

--- script.js
+++ script.js
@@ -1,15 +1,53 @@
-const button = document.querySelector(".addtocart");
-const done = document.querySelector(".done");
-console.log(button);
-let added = false;
-button.addEventListener('click',()=>{
-  if(added){
-    done.style.transform = "translate(-110%) skew(-40deg)";
-    added = false;
-  }
-  else{
-    done.style.transform = "translate(0px)";
-    added = true;
-  }
-    
+var black = document.getElementById("black");
+var blueSwatch = document.getElementById("blue");
+var oliveSwatch = document.getElementById("olive");
+var pinkSwatch = document.getElementById("pink");
+var whiteSwatch = document.getElementById("white");
+var navySwatch = document.getElementById("navy");
+var image = document.getElementById("photo");
+var price = document.getElementById("price");
+
+black.addEventListener("mouseover", function() {
+  console.log("Black");
+  image.src = "img/black.png";
+  price.innerHTML = "$159.99";
+  price.style.color = "red";
+  price.innerHTML += " (Sale)";
 });
+
+
+blueSwatch.addEventListener("mouseover", function() {
+  console.log("Blue");
+  image.src = "img/blue.png";
+  price.innerHTML = "$159.99";
+  price.style.color = "red";
+  price.innerHTML += " (Sale)";
+});
+
+oliveSwatch.addEventListener("mouseover", function() {
+  image.src = "img/green.png";
+  price.innerHTML = "$169.99";
+  price.style.color = "darkgreen";
+});
+
+pinkSwatch.addEventListener("mouseover", function() {
+  image.src = "img/pink.png";
+  price.innerHTML = "$89.99";
+  price.style.color = "green";
+  price.innerHTML += " (Clearance)";
+});
+
+whiteSwatch.addEventListener("mouseover", function() {
+  image.src = "img/white.png";
+  price.innerHTML = "$109.99";
+  price.style.color = "green";
+  price.innerHTML += " (Clearance)";
+});
+
+navySwatch.addEventListener("mouseover", function() {
+  image.src = "img/navy.png";
+  price.innerHTML = "$109.99";
+  price.style.color = "green";
+  price.innerHTML += " (Clearance)";
+});
+
